f935e0bfe8e4c82710318ec732f43659e5f3455c
r3b0rn <yakovinart123@gmail.com>
2026-01-18 17:01:24 +0300

    ✨ feat: added main panel display in `cli/panel.py`

---
+++ cli/panel.py
@@ -0,0 +1,58 @@
+import tomllib
+from pathlib import Path
+from types import SimpleNamespace
+from typing import TYPE_CHECKING
+
+import rich
+from rich.console import Group
+from rich.panel import Panel
+from rich.style import Style
+from rich.table import Table
+from rich.text import Text
+
+if TYPE_CHECKING:
+    from cli.app import RunConfig
+
+
+STYLES = SimpleNamespace(
+    primary=Style(color="#bcbec4"),
+    secondary=Style(color="#6d6e73")
+)
+
+
+def _get_version() -> str:
+    pyproject_path = Path("pyproject.toml")
+
+    with pyproject_path.open("rb") as f:
+        data = tomllib.load(f)
+        return data['project']['version']
+
+
+def show_main_panel(cfg: 'RunConfig') -> None:
+    header = Table.grid(expand=True)
+
+    header.add_column(justify="left", no_wrap=True)
+    header.add_column(justify="right", no_wrap=True)
+    header.add_row(
+        Text.assemble((">_ ", STYLES.secondary), ("gpt-agent", "bold white"),),
+        Text(f"(v{_get_version()})", style=STYLES.secondary),
+    )
+
+    table = Table.grid(expand=True)
+
+    table.add_column(justify="left", no_wrap=True)
+    table.add_column(justify="right")
+    table.add_row(
+        Text("provider:", style=STYLES.secondary),
+        Text(cfg.provider, style=STYLES.primary)
+    )
+    table.add_row(
+        Text("model:", style=STYLES.secondary),
+        Text(cfg.model, style=STYLES.primary)
+    )
+    table.add_row(
+        Text("browser:", style=STYLES.secondary),
+        Text(cfg.browser, style=STYLES.primary)
+    )
+
+    rich.print(Panel(Group(header, Text(""), table), width=40))
