724d798945935ada04aca005ccb4292e6fa7a73e
r3b0rn <yakovinart123@gmail.com>
2026-01-18 17:31:19 +0300

    ✨ feat: added cli/consts.py with text styles

---
+++ cli/consts.py
@@ -0,0 +1,10 @@
+from types import SimpleNamespace
+
+from rich.style import Style
+
+
+STYLES = SimpleNamespace(
+    primary=Style(color="#bcbec4"),
+    secondary=Style(color="#6d6e73"),
+    warning=Style(color="#cc3300"),
+)
