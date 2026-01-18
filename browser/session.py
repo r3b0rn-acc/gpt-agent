7cfef2e3c8a9208974df29fe617864778b52d7fb
r3b0rn <yakovinart123@gmail.com>
2026-01-18 15:00:35 +0300

    ✨ feat: added BrowserSession class with context manager methods

---
+++ browser/session.py
@@ -0,0 +1,10 @@
+class BrowserSession:
+    def __init__(self):
+        ...
+
+    def __enter__(self):
+        ...
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        ...
